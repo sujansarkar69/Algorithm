55489ef995434fd7091230682180ae0facd439ef
sujansarkar69 <tamim1995.itk@gmail.com>
2023-03-09 20:27:49 +0600

    first commit

--- week 01 Graph Algorithm/Lab module/1. Adjacency matrix.cpp
+++ week 01 Graph Algorithm/Lab module/1. Adjacency matrix.cpp
@@ -26,6 +26,12 @@ matrix[i][j] = 1,
 3 -> 1
 1 -> 3
 
+
+0 1 0 0
+1 0 1 1
+0 1 0 1
+0 1 1 0
+
 */
 
 #include <bits/stdc++.h>
@@ -42,7 +48,22 @@ int main()
             matrix[i][j] = 0;
         }
     }
-    
+
+    matrix[0][1] = 1;
+    matrix[1][0] = 1;
+
+    matrix[1][2] = 1;
+    matrix[2][1] = 1;
+
+    matrix[1][2] = 1;
+    matrix[2][1] = 1;
+
+    matrix[2][3] = 1;
+    matrix[3][2] = 1;
+
+    matrix[3][1] = 1;
+    matrix[1][3] = 1;
+
     for (int i = 0; i < nodes; i++)
     {
         for (int j = 0; j < nodes; j++)
