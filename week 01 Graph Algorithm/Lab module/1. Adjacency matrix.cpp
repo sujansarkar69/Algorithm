131c4ede6c66b9d1bd03ba584a258631ec05905a
sujansarkar69 <tamim1995.itk@gmail.com>
2023-03-09 20:18:36 +0600

    first commit

---
+++ week 01 Graph Algorithm/Lab module/1. Adjacency matrix.cpp
@@ -0,0 +1,56 @@
+/*
+Graph Representation:
+=====================
+Topic: Adjacency Matrix: 2D array
+
+0 ---- 1
+      / \
+     2---3
+
+#total nodes = 4
+#total edge = 4
+#undirected + unweighted
+
+matrix -> number of nodes x number of nodes
+       -> 4x4
+
+matrix[i][j] = 1,
+    if and only if i can go to j in one directed line (i->j)
+
+0 -> 1
+1 -> 0
+1 -> 2
+2 -> 1
+2 -> 3
+3 -> 2
+3 -> 1
+1 -> 3
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int nodes = 4;
+    int matrix[nodes][nodes];
+
+    for (int i = 0; i < nodes; i++)
+    {
+        for (int j = 0; j < nodes; j++)
+        {
+            matrix[i][j] = 0;
+        }
+    }
+    
+    for (int i = 0; i < nodes; i++)
+    {
+        for (int j = 0; j < nodes; j++)
+        {
+            cout << matrix[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
