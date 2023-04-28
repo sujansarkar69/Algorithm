e7a36cc707e517da9d4c8520cfbd6377d7905980
sujansarkar69 <tamim1995.itk@gmail.com>
2023-04-29 01:03:13 +0600

    first commit

--- Phitron Exam/Lab 02/4. nth tribonacci memoization.cpp
+++ Phitron Exam/Lab 02/4. nth tribonacci memoization.cpp
@@ -1,14 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int>dp(38, 0);
 class Solution {
 public:
-    int dp[38];
+    // int dp[38];
     int tribonacci(int n) {
     if(n == 0) return 0;
-    if(n == 1) return 1;
-    if(n == 2) return 1;
+    if(n <= 2) return 1;
 
     if(dp[n] != 0) return dp[n];
 
-    dp[n] = tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
-    return dp[n];
+    return dp[n] = tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
     }
 };
+
+int main()
+{
+    Solution s;
+    int n;
+    cin >> n;
+    cout << s.tribonacci(n) << endl;
+    return 0;
+}
