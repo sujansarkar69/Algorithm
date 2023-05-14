190d842e78059f3ef9d1586fc8f13bc03f31da73
sujansarkar69 <tamim1995.itk@gmail.com>
2023-05-14 17:34:46 +0600

    first commit

--- Phitron Exam/Lab final/6. Back to the world.cpp
+++ Phitron Exam/Lab final/6. Back to the world.cpp
@@ -1,92 +1,79 @@
-// Problem link: https://lightoj.com/problem/back-to-underworld
-
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long 
 
-vector<int> vec[20009]; 
-bool vis[20009] = {};
-bool node_type[20009] = {};
+const int N = 20010;
+vector<int> adj_list[N];
+int mark[N];
+int color[N];
 
-long long int bfs(int x);
-
-
-void clearing()
+enum
 {
-    memset(vis, false, sizeof(vis));
-    memset(node_type, false, sizeof(node_type));
-    for (int i = 0; i < 20009; i++)
-        vec[i].clear();
-}
+    NOT_VISITED,
+    BLACK,
+    RED
+};
 
-long long int modified_bfs(int x)
+int bfs(int src)
 {
-    long long int lykan_num = 0, vampire_num = 0;
     queue<int> q;
-
-    q.push(x);
-    vis[x] = 1;
-    node_type[x] = 1;
-    ++lykan_num;
-
+    ll black = 0, red = 0;
+    q.push(src);
+    color[src] = BLACK;
+    black++;
     while (!q.empty())
     {
         int u = q.front();
         q.pop();
-
-        for (int i = 0; i < vec[u].size(); i++)
+        for (int i = 0; i < adj_list[u].size(); i++)
         {
-            int v = vec[u][i];
-            if (vis[v] == 0)
+            int v = adj_list[u][i];
+            if (color[v] == NOT_VISITED)
             {
                 q.push(v);
-                vis[v] = 1;
-
-                if (node_type[u] == 1) 
+                if (color[u] == BLACK)
                 {
-                    node_type[v] = 0;
-                    ++vampire_num;
+                    color[v] = RED;
+                    red++;
                 }
                 else
                 {
-                    node_type[v] = 1;
-                    ++lykan_num;
+                    color[v] = BLACK;
+                    black++;
                 }
             }
         }
     }
-
-    long long int mx = max(vampire_num, lykan_num);
-
-    return mx;
+    return max(black, red);
 }
 
 int main()
 {
-    int T, u, v, n, case_num = 1;
-    scanf("%d", &T);
-
-    while (T--)
+    int t;
+    cin >> t;
+    for (int i = 1; i <= t; i++)
     {
-        clearing(); 
-
-        scanf("%d", &n);
-        long long int sum = 0;
-
-        while (n--)
+        int n;
+        cin >> n;
+        for (int i = 0; i < N; i++)
+            adj_list[i].clear();
+        for (int j = 0; j < n; j++)
         {
-            scanf("%d %d", &u, &v);
-            vec[u].push_back(v);
-            vec[v].push_back(u);
+            int u, v;
+            cin >> u >> v;
+            adj_list[u].push_back(v);
+            adj_list[v].push_back(u);
         }
 
-        for (int i = 0; i < 20009; i++)
+        ll sum = 0;
+        memset(color, 0, sizeof(color));
+
+        for (int j = 0; j < 20005; j++)
         {
-            if (vis[i] == 0 && !vec[i].empty())
-            {
-                sum += modified_bfs(i);
-            }
+            if (color[j] == 0 and !adj_list[j].empty())
+                sum += bfs(j);
         }
-        cout << "Case " << case_num++ << ": " << sum << '\n';
+        cout << "Case " << i << ": " << sum << "\n";
     }
     return 0;
 }
